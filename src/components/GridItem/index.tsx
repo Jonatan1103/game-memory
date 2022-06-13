7e121d56bfbc403e6638b7e1396f88e7f9223ce5
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-06-13 10:27:59 -0300

    initial timer

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -11,9 +11,13 @@ type Props = {
 
 const GridItem = ({item, onClick}: Props) => {
   return(
-    <C.Container onClick={onClick}>
+    <C.Container showBackground={item.permanentShown || item.shown} onClick={onClick}>
       {item.permanentShown === false && item.shown === false &&
-        <C.Icon src={b7webIcon} alt='Logo da empresa'/>
+        <C.Icon 
+          src={b7webIcon} 
+          alt='Logo da empresa'
+          opacity={.3}
+        />
       }
 
       {(item.permanentShown || item.shown) && item.item !== null &&
--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+type Props = {
+  showBackground: boolean
+}
 
-export const Icon = styled.img``
+type ImgProps = {
+  opacity?: number
+}
+
+export const Container = styled.div<Props>`
+  background-color: 
+    ${props => props.showBackground ? '#1550ff' : '#e2e3e3'};
+  height: 100px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
+
+export const Icon = styled.img<ImgProps>`
+  width: 40px;
+  height: 40px;
+  opacity: ${props => props.opacity ?? 1};
+`
